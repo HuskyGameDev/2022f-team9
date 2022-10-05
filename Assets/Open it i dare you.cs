e5d6ab48a688ca39166d82ff67c1d44c936ca41f
Matthew Dunham <mrdunham@mtu.edu>
2022-10-05 17:45:31 -0400

    added script

---
+++ Assets/Open it i dare you.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openitidareyou : MonoBehaviour {
+    //https://www.youtube.com/watch?v=iik25wqIuFo
+}
