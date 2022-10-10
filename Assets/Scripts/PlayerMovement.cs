ca7d7338df1b908761801701b2f86e3296655991
Matthew Dunham <mrdunham@mtu.edu>
2022-10-09 21:01:17 -0400

    Added more complex movement
    Changed to a more complex movement system and the controls so far are just a and d for left and right respectively, space to jump, and left shift to crouch. crouching does lower the hitbox but reduces speed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,61 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
-    [SerializeField] private LayerMask groundLayer;
-    //[SerializeField] private LayerMask wallLayer;
-    private Rigidbody2D body;
-    //private Animator anim;
-    private BoxCollider2D boxCollider;
-    //private float wallJumpCooldown;
-    private float horizontalInput;
 
-    private void Awake() {
-        //Grab references for rigidbody and animator from object
-        body = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
+    public PlayerController controller;
 
-    private void Update() {
+    public float runSpeed = 40f;
 
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-        if (Input.GetKey(KeyCode.Space)) {
-            Jump();
+    float horizontalDir = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update() {
+
+        horizontalDir = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
         }
 
-        //Flip player when moving left-right
-        if (horizontalInput > 0.01f) {
-            transform.localScale = Vector3.one;
-        } else if (horizontalInput < -0.01f) {
-            transform.localScale = new Vector3(-1, 1, 1);
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
         }
 
-        //Set animator parameters
-        /*anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", isGrounded());
-*/
     }
 
-    private void Jump() {
-        if (isGrounded()) {
-            body.velocity = new Vector2(body.velocity.x, jumpPower);
-            //anim.SetTrigger("jump");
-        } /*else if (onWall() && !isGrounded()) {
-            if (horizontalInput != 1 && horizontalInput != -1) {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 6);
-                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            } else {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-            }
-            wallJumpCooldown = 0;
-        }*/
-    }
+    private void FixedUpdate() {
 
-    private bool isGrounded() {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        controller.Move(horizontalDir * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+
+
     }
 }
