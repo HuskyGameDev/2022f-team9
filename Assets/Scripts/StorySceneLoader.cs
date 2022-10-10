569853dd3d9a9cd868242a47d1c3647ed5850028
Rosario Curcuru <112356607+rjcurcuru@users.noreply.github.com>
2022-10-10 17:49:23 -0400

    intro scene

---
+++ Assets/Scripts/StorySceneLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StorySceneLoader : MonoBehaviour
+{
+    void OnEnable()
+    {
+        SceneManager.LoadScene("Sample Scene", LoadSceneMode.Single);
+    }
+}
