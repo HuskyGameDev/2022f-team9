0c5cc44a90c077c4aed3a0e9295bb5afc9a57fa0
Rosario Curcuru <112356607+rjcurcuru@users.noreply.github.com>
2022-10-11 16:58:30 -0400

    Added a button to tutorial

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public bool isPressed;
+
+    public void PressButton()
+    {
+        if (!isPressed)
+        {
+            isPressed = true;
+            Debug.Log("Button was pressed");
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInv.cs
+++ Assets/Scripts/Player/PlayerInv.cs
@@ -1,9 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInv : MonoBehaviour {
 
     public bool hasKey;
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
 
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            Debug.Log("Player not in range");
+        }
+    }
 }
