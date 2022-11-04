609b0a6e4755a008c56201396faca9fbbbe86bc3
Connor <62310912+connor1221@users.noreply.github.com>
2022-11-03 22:56:34 -0400

    Update ButtonController.cs

--- Assets/Scripts/Generics/Button/ButtonController.cs
+++ Assets/Scripts/Generics/Button/ButtonController.cs
@@ -17,67 +17,8 @@ public class ButtonController : MonoBehaviour
         }
     }
 
-    //Level 1 Button order = Red Yellow Blue Green
-
-    public void pressRed() {
-
-	lc.buts[0] = true;
-	PressButton();
-
-    }
-
-    public void pressYellow() {
-
-	if(lc.buts[0]) {
-	    lc.buts[1] = true;
-	    PressButton();
-	}
-	else {
-	    reset();
-	}
-    }
-
-    public void pressBlue() {
-
-	if(lc.buts[1]) {
-	    lc.buts[2] = true;
-	    PressButton();
-	}
-	else {
-	    reset();
-	}
-
-    }
-
-    public void pressGreen() {
-
-	if(lc.buts[2]) {
-	    lc.buts[3] = true;
-	    PressButton();
-	}
-	else {
-	    reset();
-	}
-
-    }
-
-    private void Update() {
-
-	if(!lc.buts[0]) {
-	    isPressed = false;
-	    gameObject.GetComponent<SpriteRenderer>().sprite = Off;
-	}
-
-    }
-
     private void reset() {
-
-	for(int i = 0; i < 4; i++)
-	{
-	    lc.buts[i] = false;
-	}
 	isPressed = false;
 	gameObject.GetComponent<SpriteRenderer>().sprite = Off;
     }
-
 }
