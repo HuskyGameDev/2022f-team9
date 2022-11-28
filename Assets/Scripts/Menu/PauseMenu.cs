ee27e862706d07044d0b8ac6647f36c7f64dfe99
Rosario Curcuru <112356607+rjcurcuru@users.noreply.github.com>
2022-11-28 16:04:49 -0500

    bug fixes and pause menu prefab

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -38,10 +38,12 @@ public class PauseMenu : MonoBehaviour
     }
     public void LoadMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
     public void Restart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
