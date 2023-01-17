5e352c9bcfe1311619563f9cc71eaed927455167
Rosario Curcuru <112356607+rjcurcuru@users.noreply.github.com>
2023-01-17 13:28:46 -0500

    moving platforms in box level

--- Assets/Scripts/BoxLevel/Platform.cs
+++ Assets/Scripts/BoxLevel/Platform.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    public Transform position1, position2;
+    public float speed;
+    public Transform startPos;
+
+    Vector3 nextPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextPos = startPos.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position == position1.position)
+        {
+            nextPos = position2.position;
+        }
+        if (transform.position == position2.position)
+        {
+            nextPos = position1.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(position1.position, position2.position);
     }
 }
