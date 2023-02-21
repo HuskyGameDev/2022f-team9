f06e6d0ccf51ecda85007f121a89830ab3b83e6b
Rosario Curcuru <112356607+rjcurcuru@users.noreply.github.com>
2023-02-21 13:14:09 -0500

    began working on buttons in laser level

--- Assets/Scripts/Laser Level/LaserMovement.cs
+++ Assets/Scripts/Laser Level/LaserMovement.cs
@@ -4,5 +4,41 @@ using UnityEngine;
 
 public class LaserMovement : MonoBehaviour
 {
-    
+    public Transform position1, position2;
+    public float speed;
+    public Transform startPos;
+
+    [SerializeField] ButtonController buttonController;
+
+    Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!buttonController.isPressed)
+        {
+            if (transform.position == position1.position)
+            {
+                nextPos = position2.position;
+            }
+            if (transform.position == position2.position)
+            {
+                nextPos = position1.position;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(position1.position, position2.position);
+    }
 }
+
+
