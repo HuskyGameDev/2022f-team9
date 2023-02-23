631bbb33b58d7b49394a72bb46248692552e3139
aarouns <aarouns@gmail.com>
2023-02-22 19:34:32 -0500

    Number puzzle level
    puzzle mechanics nearly done, incorporating into level design next.

---
+++ Assets/Scripts/NumPuzzleLevel/PuzzleScript.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+
+public class PuzzleScript : MonoBehaviour
+{
+
+    [SerializeField] private Transform emptySpace;
+    private int emptySpaceIndex = 15;
+    private Camera _camera;
+
+    [SerializeField] private TileScript[] tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        Shuffle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            //when raycast hits object the object is stored in variable hit
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                if(Vector2.Distance(emptySpace.position, hit.transform.position) < 4)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    TileScript thisTile = hit.transform.GetComponent<TileScript>();
+                    //swap positons of empty space and current hit
+                    emptySpace.position = thisTile.targetPosition;
+                    thisTile.targetPosition = lastEmptySpacePosition;
+                    int tileIndex = findIndex(thisTile);
+                    tiles[emptySpaceIndex] = tiles[tileIndex];
+                    tiles[tileIndex] = null;
+                    emptySpaceIndex = tileIndex;
+                }
+            }
+
+        }
+    }
+
+    public void Shuffle()
+    {
+        if (emptySpaceIndex != 15)
+        {
+            var tileOn15LastPos = tiles[15].targetPosition;
+            tiles[15].targetPosition = emptySpace.position;
+            emptySpace.position = tileOn15LastPos;
+            tiles[emptySpaceIndex] = tiles[15];
+            tiles[15] = null;
+            emptySpaceIndex = 15;
+        }
+        int inversion;
+        do
+        {
+            for (int i = 0; i <= 14; i++)
+            {
+                var lastPos = tiles[i].targetPosition;
+                int randomIndex = Random.Range(0, 14);
+                tiles[i].targetPosition = tiles[randomIndex].targetPosition;
+                tiles[randomIndex].targetPosition = lastPos;
+                //changing index of puzzle tile in array also
+                var tile = tiles[i];
+                tiles[i] = tiles[randomIndex];
+                tiles[randomIndex] = tile;
+
+            }
+            inversion = GetInversions();
+            Debug.Log("Puzzle shuffled");
+        } while (inversion % 2 != 0);
+
+    }
+
+    public int findIndex(TileScript ts)
+    {
+        for(int i = 0; i < tiles.Length; i++)
+        {
+            if (tiles[i] != null)
+            {
+                if(tiles[i] == ts)
+                {
+                    return i;
+                }
+            }
+        }
+        return -1;
+
+    }
+
+
+    int GetInversions()
+    {
+        int inversionsSum = 0;
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            int thisTileInvertion = 0;
+            for (int j = i; j < tiles.Length; j++)
+            {
+                if (tiles[j] != null)
+                {
+                    if (tiles[i].number > tiles[j].number)
+                    {
+                        thisTileInvertion++;
+                    }
+                }
+            }
+            inversionsSum += thisTileInvertion;
+        }
+        return inversionsSum;
+    }
+}
---
+++ Assets/Scripts/NumPuzzleLevel/TileScript.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+
+    public Vector3 targetPosition;
+    private Vector3 correctPosition;
+    private SpriteRenderer _sprite;
+
+    public int number;
+
+    void Awake()
+    {
+        targetPosition = transform.position;
+        correctPosition = transform.position;
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition,0.05f);
+        if(targetPosition == correctPosition)
+        {
+            _sprite.color = Color.green;
+        }
+        else
+        {
+            _sprite.color = Color.white;
+        }
+    }
+}
