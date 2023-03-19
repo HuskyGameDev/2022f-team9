eeb81da41f65cdd6bf92822deb0dd93b9e4bcfac
Mack Abercrombie <mdabercr@mtu.edu>
2023-03-19 19:43:24 -0400

    Main weight level components done

--- Assets/Scripts/WeightLevel/WeightMeasurment.cs
+++ Assets/Scripts/WeightLevel/WeightMeasurment.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class WeightMeasurment : MonoBehaviour
 {
-    //can probably repurpose this for waypoints since no longer needed
+    //Array of all possible locations for the bridge
     [SerializeField] private GameObject[] weightWaypoints;
-    //[SerializeField] private float speed = 2f;
-    //the total weight that is calculated
+    //How fast the bridge will move
+    [SerializeField] private float speed = 2f;
+    //The total weight that is calculated
     public int totalWeight;
 
     //Each collider for each box
@@ -27,13 +28,14 @@ public class WeightMeasurment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Calcultes the correct total weight
         CalculateWeight();
-
-        //After this move the platform based on the totalWeight
+        //Moves the bridge based on the total weight
+        BridgeMovement();
 
     }
 
+    //Calculates the correct total weight
     void CalculateWeight()
     {
 
@@ -63,11 +65,83 @@ public class WeightMeasurment : MonoBehaviour
 
     }
 
+    //Moves the bridge based on the current total weight
     void BridgeMovement()
     {
 
-        //code for moving bridge here
-        //look at notecards for code
+        if (totalWeight == 0)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[0].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 2)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[1].transform.position, Time.deltaTime * speed);
+
+        }
+        else if (totalWeight == 6)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[2].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 7)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[3].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 8)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[4].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 9)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[5].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 13)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[6].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 15)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[7].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 19)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[8].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 20)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[9].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 21)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[10].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 22)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[11].transform.position, Time.deltaTime * speed);
+
+        } else if (totalWeight == 26)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[12].transform.position, Time.deltaTime * speed);
+
+        } else
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, weightWaypoints[13].transform.position, Time.deltaTime * speed);
+
+        }
 
     }
+
 }
