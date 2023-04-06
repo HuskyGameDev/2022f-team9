538db922ef43cd82580efd3749d54c681f82f8bd
Mack Abercrombie <mdabercr@mtu.edu>
2023-04-06 18:24:21 -0400

    Small placeholder sprites and button functionality

--- Assets/Scripts/WeightLevel/Buttons.cs
+++ Assets/Scripts/WeightLevel/Buttons.cs
@@ -7,12 +7,13 @@ public class Buttons : MonoBehaviour
 {
     //true if player is in range, false otherwise
     public bool isInRange;
-
     //key the player has to press to press the button
     public KeyCode interactKey;
-
     //the action that the button performs
     public UnityEvent interactAct;
+    public Sprite on;
+    public Sprite off;
+    public bool isPressed;
 
     // Update is called once per frame
     void Update()
@@ -20,10 +21,12 @@ public class Buttons : MonoBehaviour
 
         if (isInRange)
         {
-            if (Input.GetKeyDown(interactKey))
+            if (Input.GetKeyDown(interactKey) && !isPressed)
             {
+                isPressed = true;
+                gameObject.GetComponent<SpriteRenderer>().sprite = on;
                 interactAct.Invoke();
-
+                StartCoroutine(delayReset());
             }
         }
 
@@ -43,5 +46,17 @@ public class Buttons : MonoBehaviour
         {
             isInRange = false;
         }
+    }
+
+    private void reset()
+    {
+        isPressed = false;
+        gameObject.GetComponent<SpriteRenderer>().sprite = off;
+    }
+
+    IEnumerator delayReset()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        reset();
     }
 }
