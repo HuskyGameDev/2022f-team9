c8077296ad1342e8a2078ddaa18d9494e44b58e0
Matthew Dunham <mrdunham@mtu.edu>
2023-04-10 18:24:31 -0400

    Added Player Animations
    also made a player prefab that will have to be brought into all levels now *sigh*

--- Assets/Scripts/Generics/Player/PlayerMovement.cs
+++ Assets/Scripts/Generics/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public PlayerController controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -12,18 +13,20 @@ public class PlayerMovement : MonoBehaviour {
     public bool canJump;
     bool jump = false;
     bool crouch = false;
+    float vertVel = 0f;
 
     private void Start() {
         canJump = true;
     }
 
     void Update() {
-
         horizontalDir = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalDir));
 
         if (Input.GetButtonDown("Jump") && canJump) {
             jump = true;
         }
+        animator.SetFloat("vertVelocity", GetComponentInParent<Rigidbody2D>().velocity.y);
 
         if (Input.GetButtonDown("Crouch")) {
             crouch = true;
